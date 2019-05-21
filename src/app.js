6d04f88aa2110e272007cac15b4396e997290712
misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-05-21 10:25:05 +0500

    almost done

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const getData = require("./getData")
+var IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
+
+const publicFolderPath = path.join(__dirname, '../public')
+app.use(express.static(publicFolderPath));
+
+app.set("view engine", "hbs")
+
+const viewFolderPath = path.join(__dirname, '../templates/views')
+app.set("views", viewFolderPath)
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/search", (req, res) => {
+    const { city } = req.query;
+    if (city) {
+        getData(res, city);
+    }
+    else {
+        var ipgeolocationApi = new IPGeolocationAPI("85c4e3caca5c4d8abcd4bca8173284d7", false);
+        ipgeolocationApi.getGeolocation((res1) => {
+            if (res1) {
+                getData(res, res1.city)
+            }
+            else {
+                return res.send({error: "Sorry, something went wrong!"})
+            }
+        })
+    }
+})
+const port = 3030
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
---
+++ src/getData.js
@@ -0,0 +1,54 @@
+const request = require("request");
+
+const getData = (check, city) => {
+    let data = {};
+    const options = {
+        url: ` https://api.apixu.com/v1/current.json?key=f3202b9d9ea34f868f3120440180206&q=${city}`,
+        json: true,
+    }
+    request(options, (err, response) => {
+        if (response) {
+            if (response.body.error) {
+                check.send({error: response.body.error.message}) 
+            }
+            data.country = response.body.location.country;
+            data.region = response.body.location.region;
+            data.temp = response.body.current.temp_c;
+            if (response.body.location.country) {
+                const newAPIKey = "89b7784face7425d8fd5a69979a3bbfa"
+                const newsLink = `https://newsapi.org/v2/top-headlines?q=${response.body.location.country}&
+            sources=bbc-news&apiKey=${newAPIKey}`
+                const optionsForNews = {
+                    url: newsLink,
+                    method: "GET",
+                    json: true,
+                }
+                request(optionsForNews, (err, res) => {
+                    if (res && res.body.status === "ok") {
+                        if (res.body.totalResults !== 0) {
+                            const temarr = res.body.articles.map(v => {
+                                return {title: v.title, source: v.source.name, des: v.description}
+                            })
+                            data.news = temarr;
+                            check.send(data)
+                        }
+                        else {
+                            check.send({error:  "Sorry, No Top News"})
+                        }
+                    }
+                    else {
+                        check.send({error: "Sorry, something went wrong"})
+                    }
+                })
+            }
+            else {
+                check.send({error : "Sorry, something went wrong"})
+            }
+        }
+        else {
+            check.send({error : "Sorry, something went wrong"})
+        }
+    })
+}
+
+module.exports = getData;
